bb7508c0936d2fe31e3cce70fadba81fddf5593a
Billy Zhu <billyzhu@dhcp-rhodes-2940.eduroam.cornell.edu>
2018-04-10 23:00:38 -0400

    got hovering working

---
+++ website (merged)/scripts/bar_chart.js
@@ -0,0 +1,92 @@
+function hover(){
+  d3.selectAll(".alcohol_use").on("mouseover", function(){
+    d3.selectAll(".alcohol_use").style("opacity", 1);
+  })
+  d3.selectAll(".alcohol_use").on("mouseout", function(){
+    d3.selectAll(".alcohol_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".marijuana_use").on("mouseover", function(){
+    d3.selectAll(".marijuana_use").style("opacity", 1);
+  })
+  d3.selectAll(".marijuana_use").on("mouseout", function(){
+    d3.selectAll(".marijuana_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".cocaine_use").on("mouseover", function(){
+    d3.selectAll(".cocaine_use").style("opacity", 1);
+  })
+  d3.selectAll(".cocaine_use").on("mouseout", function(){
+    d3.selectAll(".cocaine_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".crack_use").on("mouseover", function(){
+    d3.selectAll(".crack_use").style("opacity", 1);
+  })
+  d3.selectAll(".crack_use").on("mouseout", function(){
+    d3.selectAll(".crack_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".heroin_use").on("mouseover", function(){
+    d3.selectAll(".heroin_use").style("opacity", 1);
+  })
+  d3.selectAll(".heroin_use").on("mouseout", function(){
+    d3.selectAll(".heroin_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".hallucinogen_use").on("mouseover", function(){
+    d3.selectAll(".hallucinogen_use").style("opacity", 1);
+  })
+  d3.selectAll(".hallucinogen_use").on("mouseout", function(){
+    d3.selectAll(".hallucinogen_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".inhalant_use").on("mouseover", function(){
+    d3.selectAll(".inhalant_use").style("opacity", 1);
+  })
+  d3.selectAll(".inhalant_use").on("mouseout", function(){
+    d3.selectAll(".inhalant_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".pain_reliever_use").on("mouseover", function(){
+    d3.selectAll(".pain_reliever_use").style("opacity", 1);
+  })
+  d3.selectAll(".pain_reliever_use").on("mouseout", function(){
+    d3.selectAll(".pain_reliever_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".oxycontin_use").on("mouseover", function(){
+    d3.selectAll(".oxycontin_use").style("opacity", 1);
+  })
+  d3.selectAll(".oxycontin_use").on("mouseout", function(){
+    d3.selectAll(".oxycontin_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".tranquilizer_use").on("mouseover", function(){
+    d3.selectAll(".tranquilizer_use").style("opacity", 1);
+  })
+  d3.selectAll(".tranquilizer_use").on("mouseout", function(){
+    d3.selectAll(".tranquilizer_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".stimulant_use").on("mouseover", function(){
+    d3.selectAll(".stimulant_use").style("opacity", 1);
+  })
+  d3.selectAll(".stimulant_use").on("mouseout", function(){
+    d3.selectAll(".stimulant_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".meth_use").on("mouseover", function(){
+    d3.selectAll(".meth_use").style("opacity", 1);
+  })
+  d3.selectAll(".meth_use").on("mouseout", function(){
+    d3.selectAll(".meth_use").style("opacity", 0.5);
+  })
+
+  d3.selectAll(".sedative_use").on("mouseover", function(){
+    d3.selectAll(".sedative_use").style("opacity", 1);
+  })
+  d3.selectAll(".sedative_use").on("mouseout", function(){
+    d3.selectAll(".sedative_use").style("opacity", 0.5);
+  })
+}
